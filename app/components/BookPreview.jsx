d72a6918b923068bbc9e1bbcd0ea0603405c83c0
mtso <mtso@tuta.io>
2017-06-24 14:50:01 -0700

    Resolve book load

--- app/components/BookPreview.jsx
+++ app/components/BookPreview.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import Modal from './Modal'
 
-const BookPreview = ({ history, id, title, image_url, display_name }) => (
-  <Modal>
-    <button onClick={() => history.goBack()}>Close</button>
-    <br />
-    <img src={image_url} />
-    <br />
-    { title }
-    <br />
-    { display_name }
-  </Modal>
-)
+const BookPreview = ({ history, id, title, image_url, owner }) => {
+  if (!owner) {
+    return (
+      <Modal>
+        <button onClick={() => {
+          if (history.length < 3) {
+            history.push('/')
+          } else {
+            history.goBack()
+          }
+        }}>Close</button>
+      </Modal>
+    )
+  }
+  const { display_name } = owner
+  return (
+    <Modal>
+      <button onClick={() => {
+        if (history.length < 3) {
+          history.push('/')
+        } else {
+          history.goBack()
+        }
+      }}>Close</button>
+      <br />
+      <img src={image_url} />
+      <br />
+      { title }
+      <br />
+      { display_name }
+    </Modal>
+  )
+}
 
 export default BookPreview
--- server/models/book.go
+++ server/models/book.go
@@ -99,7 +99,7 @@ func ConnectBooks(conn *sql.DB) (err error) {
 // [1] count
 func (s BookSchema) GetBooks(page ...int) ([]Book, error) {
 	offset := 0
-	count := 1000
+	count := 1000000
 
 	if len(page) > 0 {
 		offset = page[0]
